58a402572baa1e807e706617ed218e8f9a9d4a99
error-adi <adiagarwal555@gmail.com>
2026-05-01 12:19:41 +0530

    cv final fix

--- backend/routes/siteSettingsRoutes.js
+++ backend/routes/siteSettingsRoutes.js
@@ -18,6 +18,8 @@ const storage = new CloudinaryStorage({
       return {
         folder: 'portfolio/files',
         allowed_formats: ['pdf'],
+        type: 'upload',
+        access_mode: 'public',
       }
     }
   }
